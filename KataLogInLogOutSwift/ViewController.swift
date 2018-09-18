87264ecbd69ce97742618ae2f73ecd0bd8aff584
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-09-18 16:31:51 +0200

    Fix linter

--- KataLogInLogOutSwift/ViewController.swift
+++ KataLogInLogOutSwift/ViewController.swift
@@ -1,25 +1,8 @@
-//
-//  ViewController.swift
-//  KataLogInLogOutSwift
-//
-//  Created by Pedro Vicente Gomez on 18/09/2018.
-//  Copyright © 2018 GoKarumi. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
